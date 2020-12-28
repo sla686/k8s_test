f68244b5fb34ade15f332f5e1596e28915aca7b7
Galina Zharkova <galinazarkova@MacBook-Galina.local>
2020-12-28 17:28:05 +0300

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t sla686/multi-client:latest -t sla686/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sla686/multi-server:latest -t sla686/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sla686/multi-worker:latest -t sla686/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push sla686/multi-client:latest
+docker push sla686/multi-server:latest
+docker push sla686/multi-worker:latest
+
+docker push sla686/multi-client:$SHA
+docker push sla686/multi-server:$SHA
+docker push sla686/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=sla686/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sla686/client-server:$SHA
+kubectl set image deployments/worker-deployment worker=sla686/worker-server:$SHA
